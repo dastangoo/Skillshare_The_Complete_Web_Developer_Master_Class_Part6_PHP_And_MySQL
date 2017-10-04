2db734ff1df055f120d83074a321bcedfee88840
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 17:57:36 +0330

    Arrays

--- index.php
+++ index.php
@@ -4,26 +4,94 @@
     <meta charset="utf-8">
     <title>Learn PHP and MySQL</title>
   </head>
-  <style media="screen">
-    h2 {
-      display: inline;
-    }
-  </style>
   <body>
     <?php
-      $str1 = "Hello Students";
-      $str2 = "We are willing to be a <script>alert('Stop!!!!');</script><h2>professional</h2> web developer";
-      $str3 = "good people";
+      // $month = "October";
+      // $month2 = "November";
+      // echo $month2;
 
-      // echo $str2;
+      //Indexed Array
+
+      $months = array();
+      $months[1] = "January";
+      $months[2] = "February";
+      $months[3] = "March";
+      $months[4] = "April";
+      $months[5] = "Mey";
+      $months[6] = "June";
+      $months[7] = "July";
+      $months[8] = "August";
+      $months[9] = "September";
+      $months[10] = "October";
+      $months[11] = "November";
+      $months[12] = "December";
+
+      $fruits = array();
+      $fruits[] = "Apple";
+      $fruits[] = "Banana";
+      $fruits[] = "Grapes";
+      $fruits[] = "Guava";
+
+      // Associative Array
+
+      $days = array();
+      $days['mon'] = "Monday";
+      $days['tue'] = "Tuesday";
+      $days['wed'] = "Wednesday";
+      $days['thu'] = "Thursday";
+      $days['fri'] = "Friday";
+      $days['sat'] = "Saturday";
+      $days['sun'] = "Sunday";
+
+
+
+      // Array Identifier
+      $vegtables = array("Onion", "Potato", "Tomato", "Lady Finger");
+      $employees = array("manager" => "John", "Director" => "Mark", "CEO" => "Albert" );
+
+
+      // Modifying the Arrays
+
+      $months[5] = "May";
+      $fruits[2] = "Strawberry";
+      $days['thu'] = "Friday";
+
+
+      // Adding New Elements
+
+      echo $months[1];
       echo "<br>";
-      echo htmlspecialchars($str2);
+      echo $days['mon'];
       echo "<br>";
-      echo strip_tags($str2);
+      echo $fruits[3];
+      echo "<br>";
+      echo $vegtables[2];
+      echo "<br>";
+      echo $employees['manager'];
+
+      // var_dump($months);
+      // echo "<br>";
+      // var_dump($days);
+      // echo "<br>";
+      // var_dump($fruits);
+      // echo "<br>";
+      // var_dump($vegtables);
+      // echo "<br>";
+      //
+      // var_export($months);
+      // echo "<br>";
+      // var_export($days);
+      // echo "<br>";
+      // var_export($fruits);
+      // echo "<br>";
+      // var_export($vegtables);
+      // echo "<br>";
+
+
+
+
+
 
     ?>
-    <p>This is the Simple Paragraphp</p>
-    <?php
-     ?>
   </body>
 </html>
