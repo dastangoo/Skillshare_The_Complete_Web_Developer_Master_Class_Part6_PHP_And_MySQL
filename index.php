95255d914b4bd39947043111798909ce8d8a529d
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 14:54:36 +0330

    Arithmatic, Assignment and Increment, Decrement Operators

--- index.php
+++ index.php
@@ -6,31 +6,78 @@
   </head>
   <body>
     <?php
-      $var1 = 'Hello World';
-      $var1 = '<u>Hi Students</u>';
-      $var2 = 4 + 6;
+      $var1 = 8;
+      $var2 = 9;
 
-      $var3 = 6;
-      $var4 = 7;
-      $var5 = "We&apos;re learning the PHP";
-      $var6 = 'Winterfell';
-      $ans = $var3 + $var4 + $var2 + 150;
-      $br = '<br>';
-      echo $var1;
+      // Arithmetic
+
+      $add = $var1 + $var2;
+      $sub = $var1 - 9;
+      $pro = $var2 * 7;
+      $div = $var1 / $var2;
+      $mod = $var2 % 3;
+      echo $add;
       echo "<br>";
-      echo $var2 ;
-      echo $br;
-      echo $var2;
-      echo $br;
-      echo $ans;
+      echo $sub;
+      echo "<br>";
+      echo $pro;
+      echo "<br>";
+      echo $div;
+      echo "<br>";
+      echo $mod;
 
-      echo $br;
-      echo "This is the Paragraph" . 4;
+      // Assignment Operators
+      $var3 = 4;
+      $var4 = $var3;
+      $var5 = 7;
 
-      echo $br;
-      echo $var1 . $var5;
-      echo $br;
-      echo "I live in <b>" . $var6 . "</b>";
+      echo "<br>";
+      echo $var4;
+
+      $a = 5;
+      $b = 9;
+
+      $c = 6;
+      $d = 8;
+
+      $e = 4;
+      $f = 3;
+
+      $g = 7;
+      $h = 2;
+
+      $i = 18;
+      $j = 9;
+
+      $c = $a + $b;
+      // $a = $a + $b;
+      $a += $b;
+      $c -= $d;
+      $e *= $f;
+      $g /= $h;
+      $i %= $j;
+
+      // $var5++;
+      echo "<br>";
+      echo $a;
+      echo "<br>";
+      echo $c;
+      echo "<br>";
+      echo $e;
+      echo "<br>";
+      echo $g;
+      echo "<br>";
+      echo $i;
+      echo "<br>";
+      echo $var5++;
+      echo "<br>";
+      echo ++$var5;
+      echo "<br>";
+      echo --$var5;
+      echo "<br>";
+      echo $var5--;
+      echo "<br>";
+      echo $var5;
     ?>
   </body>
 </html>
