07d4127e0f5ca80c43c3d9492e2a62c462ef0a5d
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 14:29:36 +0330

    All about Variables and Concatination

--- index.php
+++ index.php
@@ -5,16 +5,32 @@
     <title>Learn PHP and MySQL</title>
   </head>
   <body>
-    <!-- HTML Comment-->
-    Hello World
-    <br>
     <?php
-    // This is the Single Line Comment
-    # This is the Single Line Comment
-    /*
-      This is the
-      Multiline Comment 
-    */
-    echo "<br><h1>Hello World from PHP</h1>"; ?>
+      $var1 = 'Hello World';
+      $var1 = '<u>Hi Students</u>';
+      $var2 = 4 + 6;
+
+      $var3 = 6;
+      $var4 = 7;
+      $var5 = "We&apos;re learning the PHP";
+      $var6 = 'Winterfell';
+      $ans = $var3 + $var4 + $var2 + 150;
+      $br = '<br>';
+      echo $var1;
+      echo "<br>";
+      echo $var2 ;
+      echo $br;
+      echo $var2;
+      echo $br;
+      echo $ans;
+
+      echo $br;
+      echo "This is the Paragraph" . 4;
+
+      echo $br;
+      echo $var1 . $var5;
+      echo $br;
+      echo "I live in <b>" . $var6 . "</b>";
+    ?>
   </body>
 </html>
