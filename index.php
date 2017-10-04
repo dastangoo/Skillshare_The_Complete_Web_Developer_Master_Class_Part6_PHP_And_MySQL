d2c38de0295afd76e3fba9914054e98cb1d39e3f
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 15:49:14 +0330

    Switch Statement

--- index.php
+++ index.php
@@ -8,27 +8,36 @@
     <?php
       $var1 = 7;
       $var2 = 4;
-      $var3 = 7;
-      if ($var1 === $var2) {
-        echo "The First Conditional is True<br>";
-      } else {
-        echo "The First Conditional is False<br>";
-      }
-      if ($var1 > $var2) {
-        echo "The Second Conditional is True<br>";
-      } else {
-        echo "The Second Conditional is False<br>";
+      $var3 = 13;
+
+      $str = "Jon Snow";
+
+      switch ($var3) {
+        case 7:
+          echo "The First Case is True<br>";
+          break;
+        case 6:
+          echo "The Secodn Case is True<br>";
+          break;
+        default:
+          echo "The Default Case is True<br>";
+          break;
       }
 
-      // if, else and elseif
-      if ($var1 < $var3) {
-        echo "The Third Conditional is True<br>";
-      } elseif ($var1 <= $var3) {
-        echo "The Third Conditioanl&apos;s first elseif condition is true<br>";
-      } else {
-        echo "The Third Conditioanl is False<br>";
+      switch ($str) {
+        case "Jon Snow":
+          echo "The First Case is True<br>";
+          break;
+        case "Arya":
+          echo "The Second Case is True<br>";
+          break;
+        case "Daenerys":
+          echo "The Thrid Case is True<br>";
+          break;
+        default:
+          echo "The Default Case is True<br>";
+          break;
       }
-
     ?>
   </body>
 </html>
