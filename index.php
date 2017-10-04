4aa51a1e5d14916b5f7ef8e74258676fca0db37c
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 17:24:00 +0330

    String Functions

--- index.php
+++ index.php
@@ -6,25 +6,37 @@
   </head>
   <body>
     <?php
-      function calc1()
-      {
-        static $a = 1;
-        // $a++;
-        // return $a++;
-        return $a++;
-      }
+      $str1 = "Hello Students";
+      $str2 = "We are willing to became a professional web developer";
+      $replace_str =  str_replace("Hello", "Hi", $str1);
+      $str3 = "good people";
+      echo $str1;
+      echo str_word_count($str1);
+      echo "<br>";
+      echo $replace_str;
+      echo "<br>";
+      // echo str_repeat($str1."<br>", 10);
+      echo str_repeat("$str1<br>", 10);
+      echo "<br>";
+      echo stripos($str2, "Willing");
+      echo "<br>";
+      echo strpos($str2, "willing");
+      echo "<br>";
+      echo substr($str2, 27, 12);
+      echo "<br>";
+      echo substr($str2, strpos($str2, "professional"));
+      echo "<br>";
+      // String Formatting
+      echo strtoupper($str1);
+      echo "<br>";
+      echo strtolower($str1);
+      echo "<br>";
+      echo ucfirst($str3);
+      echo "<br>";
+      echo ucwords($str3);
+      
     ?>
-    <p>This is the Simple Paragraph</p>
     <?php
-      echo calc1();
-      echo "<br>";
-      echo calc1();
-      echo "<br>";
-      echo calc1();
-      echo "<br>";
-      echo calc1();
-      echo "<br>";
-      echo calc1();
     ?>
   </body>
 </html>
