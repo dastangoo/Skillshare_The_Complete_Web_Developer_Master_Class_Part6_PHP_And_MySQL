85ec18f81617f0c2b405fbd8f177c99848020688
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 14:05:01 +0330

    Integers and Strings

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-<!-- <h2>Hello World</h2> -->
 <?php
-  print "Hellow World";
-  echo "Hello World";
-?>
+  // print "Hellow World";
+  // echo "Hello World";
+  // echo "1 + 2";
+  // echo 1 + 3;
+  // echo 3 - 1;
+  // echo 3 * 2;
+  echo 3 / 2;
 
-<!-- <?="Hello World" ?> -->
+?>
