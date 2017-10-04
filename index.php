a4783ca18da8172b6ecbd1a7bd25256e8453c099
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 14:58:51 +0330

    Conditional statements and comparision operators

--- index.php
+++ index.php
@@ -6,78 +6,20 @@
   </head>
   <body>
     <?php
-      $var1 = 8;
-      $var2 = 9;
+        $var1 = 8;
+        $var2 = 9;
 
-      // Arithmetic
+        $var1 = $var2;
 
-      $add = $var1 + $var2;
-      $sub = $var1 - 9;
-      $pro = $var2 * 7;
-      $div = $var1 / $var2;
-      $mod = $var2 % 3;
-      echo $add;
-      echo "<br>";
-      echo $sub;
-      echo "<br>";
-      echo $pro;
-      echo "<br>";
-      echo $div;
-      echo "<br>";
-      echo $mod;
+        $var3 = 10;
+        $var4 = '10';
 
-      // Assignment Operators
-      $var3 = 4;
-      $var4 = $var3;
-      $var5 = 7;
-
-      echo "<br>";
-      echo $var4;
-
-      $a = 5;
-      $b = 9;
-
-      $c = 6;
-      $d = 8;
-
-      $e = 4;
-      $f = 3;
-
-      $g = 7;
-      $h = 2;
-
-      $i = 18;
-      $j = 9;
-
-      $c = $a + $b;
-      // $a = $a + $b;
-      $a += $b;
-      $c -= $d;
-      $e *= $f;
-      $g /= $h;
-      $i %= $j;
-
-      // $var5++;
-      echo "<br>";
-      echo $a;
-      echo "<br>";
-      echo $c;
-      echo "<br>";
-      echo $e;
-      echo "<br>";
-      echo $g;
-      echo "<br>";
-      echo $i;
-      echo "<br>";
-      echo $var5++;
-      echo "<br>";
-      echo ++$var5;
-      echo "<br>";
-      echo --$var5;
-      echo "<br>";
-      echo $var5--;
-      echo "<br>";
-      echo $var5;
+        if ($var1 == $var2) {
+          echo "This if statement is true";
+        }
+        if ($var3 === $var4) {
+          echo "This is second if statement";
+        }
     ?>
   </body>
 </html>
