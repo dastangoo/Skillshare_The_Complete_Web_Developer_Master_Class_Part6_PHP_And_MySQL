e91939006ac20e3d56cd149a57c25a44bb7dfc93
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 17:34:09 +0330

    HTML Specified Functions

--- index.php
+++ index.php
@@ -4,39 +4,26 @@
     <meta charset="utf-8">
     <title>Learn PHP and MySQL</title>
   </head>
+  <style media="screen">
+    h2 {
+      display: inline;
+    }
+  </style>
   <body>
     <?php
       $str1 = "Hello Students";
-      $str2 = "We are willing to became a professional web developer";
-      $replace_str =  str_replace("Hello", "Hi", $str1);
+      $str2 = "We are willing to be a <script>alert('Stop!!!!');</script><h2>professional</h2> web developer";
       $str3 = "good people";
-      echo $str1;
-      echo str_word_count($str1);
+
+      // echo $str2;
       echo "<br>";
-      echo $replace_str;
+      echo htmlspecialchars($str2);
       echo "<br>";
-      // echo str_repeat($str1."<br>", 10);
-      echo str_repeat("$str1<br>", 10);
-      echo "<br>";
-      echo stripos($str2, "Willing");
-      echo "<br>";
-      echo strpos($str2, "willing");
-      echo "<br>";
-      echo substr($str2, 27, 12);
-      echo "<br>";
-      echo substr($str2, strpos($str2, "professional"));
-      echo "<br>";
-      // String Formatting
-      echo strtoupper($str1);
-      echo "<br>";
-      echo strtolower($str1);
-      echo "<br>";
-      echo ucfirst($str3);
-      echo "<br>";
-      echo ucwords($str3);
-      
+      echo strip_tags($str2);
+
     ?>
+    <p>This is the Simple Paragraphp</p>
     <?php
-    ?>
+     ?>
   </body>
 </html>
