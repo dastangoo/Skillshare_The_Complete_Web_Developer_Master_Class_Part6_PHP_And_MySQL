d3c681546d917eb15e17d7e0cff6e32c841f6869
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 16:40:36 +0330

    Functions in Detail

--- index.php
+++ index.php
@@ -6,18 +6,46 @@
   </head>
   <body>
     <?php
-      function func1()
+      $e = 5;
+      $f = 9;
+      function func1($country)
       {
-        echo "This is coming form the Function<br>";
+        // echo "I live in $country<br>";
+        // echo "I live in ". $country . "<br>";
+        // return "I live in $country <br>";
+        $var1 = "I live in $country <br>";
+        return $var1;
       }
-      echo "A Simple Line<br>";
-      func1();
-      func1();
-      func1();
+      function calc1($b, $c)
+      {
+        $a = $b + $c;
+        return $a;
+      }
+
+      function calc2()
+      {
+        // First Method
+
+        // global $e, $f;
+        // $d = $e * $f;
+
+        // Second Method
+        $d = $GLOBALS['e'] * $GLOBALS['f'];
+
+        
+        return $d;
+      }
     ?>
     <p>This is the Simple Paragraph</p>
     <?php
-      func1();
+      // func1('Kings Landing');
+      echo func1('Kings Landing');
+      echo func1('Vale ');
+      echo calc1(10, 64);
+      echo "<br>";
+      echo calc1(58, 89);
+      echo "<br>";
+      echo calc2()
     ?>
   </body>
 </html>
