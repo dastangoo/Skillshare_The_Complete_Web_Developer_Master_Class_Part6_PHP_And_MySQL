33bd609217b7406172b004e664e1d506ecd6d94f
dastangoo <hamed.dastangoo@gmail.com>
2017-10-04 18:50:02 +0330

    Creating Variables in the URL

--- index.php
+++ index.php
@@ -3,105 +3,24 @@
   <head>
     <meta charset="utf-8">
     <title>Learn PHP and MySQL</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/jquery-color.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+
+      });
+    </script>
   </head>
   <body>
-    <?php
-      // $month = "October";
-      // $month2 = "November";
-      // echo $month2;
-
-      //Indexed Array
-
-      $months = array();
-      $months[1] = "January";
-      $months[2] = "February";
-      $months[3] = "March";
-      $months[4] = "April";
-      $months[5] = "Mey";
-      $months[6] = "June";
-      $months[7] = "July";
-      $months[8] = "August";
-      $months[9] = "September";
-      $months[10] = "October";
-      $months[11] = "November";
-      $months[12] = "December";
-
-      $fruits = array();
-      $fruits[] = "Apple";
-      $fruits[] = "Banana";
-      $fruits[] = "Grapes";
-      $fruits[] = "Guava";
-
-      // Associative Array
-
-      $days = array();
-      $days['mon'] = "Monday";
-      $days['tue'] = "Tuesday";
-      $days['wed'] = "Wednesday";
-      $days['thu'] = "Thursday";
-      $days['fri'] = "Friday";
-      $days['sat'] = "Saturday";
-      $days['sun'] = "Sunday";
-
-
-
-      // Array Identifier
-      $vegtables = array("Onion", "Potato", "Tomato", "Lady Finger");
-      $employees = array("manager" => "John", "Director" => "Mark", "CEO" => "Albert" );
-
-
-      // Modifying the Arrays
-
-      $months[5] = "May";
-      $fruits[2] = "Strawberry";
-      $days['thu'] = "Friday";
-
-
-      // Adding New Elements
-
-      echo $months[1];
-      echo "<br>";
-      echo $days['mon'];
-      echo "<br>";
-      echo $fruits[3];
-      echo "<br>";
-      echo $vegtables[2];
-      echo "<br>";
-      echo $employees['manager'];
-
-      // var_dump($months);
-      // echo "<br>";
-      // var_dump($days);
-      // echo "<br>";
-      // var_dump($fruits);
-      // echo "<br>";
-      // var_dump($vegtables);
-      // echo "<br>";
-      //
-      // var_export($months);
-      // echo "<br>";
-      // var_export($days);
-      // echo "<br>";
-      // var_export($fruits);
-      // echo "<br>";
-      // var_export($vegtables);
-      // echo "<br>";
-
-      // Foreah Loop
-
-      foreach ($months as $month ) {
-        echo "$month <br>";
-      }
-
-      // For Loop
-      
-      for ($i=0; $i < count($fruits); $i++) {
-        echo "$fruits[$i] <br>";
-      }
-
-
-
-
-    ?>
+    <div class="col-md-6">
+      <a href="receiver.php" class="btn btn-success">Send data without Variable</a>
+      <a href="receiver.php?var1=Batman" class="btn btn-success">Send data with Variable</a>
+      <a href="receiver.php?var2=Flash&var3=Wonder%20Woman" class="btn btn-success">Send data with Variable</a>      
+    </div>
   </body>
 </html>
---
+++ receiver.php
@@ -0,0 +1,14 @@
+<?php
+  if (isset($_GET['var1'])) {
+    echo $_GET['var1'] . "<br>";
+  } else {
+    echo "This page has a variable named var1 but it&apos;s not declared in the URL<br>";
+  }
+  if (isset($_GET['var2']) and isset($_GET['var3'])) {
+    echo $_GET['var2'] . "<br>" . $_GET['var3'] . "<br>";
+  } else {
+    echo "This page has a variable named var2 but it&apos;s not declared in the URL<br>";
+  }
+
+?>
+This is the Receiver Page
