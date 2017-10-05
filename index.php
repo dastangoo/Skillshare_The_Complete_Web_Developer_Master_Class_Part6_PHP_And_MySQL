443b273e447c75501c1dbe349f72e627aec058b4
dastangoo <hamed.dastangoo@gmail.com>
2017-10-05 11:07:55 +0330

    Getting Form Data in PHP using GET and POST method

--- index.php
+++ index.php
@@ -15,19 +15,56 @@
 
       });
     </script>
+    <style media="screen">
+      table {
+        margin: 50px;
+      }
+    </style>
   </head>
   <body>
-    <div class="col-md-6">
-      <a href="receiver.php" class="btn btn-success">Send data without Variable</a>
-      <a href="receiver.php?var1=Batman" class="btn btn-success">Send data with Variable</a>
-      <a href="receiver.php?var2=Flash&var3=Wonder%20Woman" class="btn btn-success">Send data with Variable</a>
+    <div class="col-md-8 col-md-offset-2" id="form">
+      <form method="post">
+        <div class="form-group">
+          <label for="">User Name</label>
+          <input type="text" class="form-control" id="username" placeholder="Insert Username" name="username">
+          <p class="help-block">Insert your registered username here.</p>
+          <input type="hidden" name="check" value="yes">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" placeholder="Insert Password" name="password">
+          <p class="help-block">Insert your password here.</p>
+        </div>
+        <input type="submit" name="submit" value="Submit" class="btn btn-primary btn-block btn-lg">
+      </form>
+    </div>
+    <br><br><br>
+    <div class="col-md-4 col-md-offset-4" id="result">
       <?php
-        // include "receiver.php";
-        // include_once "receive.php";
-        //require "receiver.php";
-        require_once "receiver.php";
-        // echo $str;
+        //if (isset($_GET['check'])) {
+        if (isset($_POST['check'])) {
       ?>
+      <table class="table table-bordered table-striped table-hover">
+        <tr>
+          <th>User Name</th>
+          <th>Password</th>
+        </tr>
+        <tr>
+          <td>
+            <?php
+              //echo $_GET['username'];
+              echo $_POST['username'];
+            ?>
+          </td>
+          <td>
+            <?php
+              // echo $_GET['password']; }
+              echo $_POST['password']; }
+            ?>
+          </td>
+        </tr>
+      </table>
+
     </div>
   </body>
 </html>
