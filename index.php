0e5139eea17b5e06833136e6a879b7b8953c8f8d
dastangoo <hamed.dastangoo@gmail.com>
2017-10-05 11:52:59 +0330

    Retrieving Data From Database

--- index.php
+++ index.php
@@ -24,6 +24,31 @@
     </script>
   </head>
   <body>
-
+  <?php
+    $sql = "SELECT * FROM users";
+    $run = mysqli_query($conn, $sql);
+    echo "<table class='table table-bordered table-striped table-hover'>
+      <tr>
+        <th>S.NO</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th>Contact Number</th>
+        <th>Date</th>
+      </tr>";
+    while ($rows = mysqli_fetch_assoc($run)) {
+      echo "
+        <tr>
+          <td>$rows[user_id]</td>
+          <td>$rows[name]</td>
+          <td>$rows[email]</td>
+          <td>$rows[password]</td>
+          <td>$rows[contact_number]</td>
+          <td>$rows[date]</td>
+        </tr>
+      ";
+    }
+    echo "</table>";
+  ?>
   </body>
 </html>
